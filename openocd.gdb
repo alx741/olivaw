ec393c21b97414b5a16cd0925521c970454c8cd5
Daniel Campoverde <alx@sillybytes.net>
2019-02-22 21:11:38 -0500

    PWM working

--- openocd.gdb
+++ openocd.gdb
@@ -7,9 +7,9 @@ set print asm-demangle on
 set backtrace limit 32
 
 # detect unhandled exceptions, hard faults and panics
-break DefaultHandler
-break HardFault
-break rust_begin_unwind
+# break DefaultHandler
+# break HardFault
+# break rust_begin_unwind
 
 # *try* to stop at the user entry point (it might be gone due to inlining)
 break main
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 #![deny(unsafe_code)]
-#![deny(warnings)]
-#![no_std]
+// #![deny(warnings)]
 #![no_main]
+#![no_std]
 
+extern crate cortex_m;
 extern crate cortex_m_rt as rt;
 extern crate panic_semihosting;
 extern crate stm32f1xx_hal as hal;
 
+// use cortex_m_semihosting::hprintln;
+
+// use cortex_m::asm;
 use hal::prelude::*;
 use hal::stm32;
 use rt::{entry, exception, ExceptionFrame};
@@ -15,11 +19,58 @@ use rt::{entry, exception, ExceptionFrame};
 fn main() -> ! {
     let p = stm32::Peripherals::take().unwrap();
 
+    let mut flash = p.FLASH.constrain();
     let mut rcc = p.RCC.constrain();
+
     let mut gpioc = p.GPIOC.split(&mut rcc.apb2);
 
     // #[cfg(feature = "stm32f103")]
     gpioc.pc13.into_push_pull_output(&mut gpioc.crh).set_low();
+
+    let clocks = rcc.cfgr.freeze(&mut flash.acr);
+
+    let mut afio = p.AFIO.constrain(&mut rcc.apb2);
+
+    // let mut gpioa = p.GPIOA.split(&mut rcc.apb2);
+    let mut gpiob = p.GPIOB.split(&mut rcc.apb2);
+
+    // TIM4
+    let c1 = gpiob.pb6.into_alternate_push_pull(&mut gpiob.crl);
+    let c2 = gpiob.pb7.into_alternate_push_pull(&mut gpiob.crl);
+    let c3 = gpiob.pb8.into_alternate_push_pull(&mut gpiob.crh);
+    let c4 = gpiob.pb9.into_alternate_push_pull(&mut gpiob.crh);
+
+    let (mut _pwm_ch1, mut _pwm_ch2, mut _pwm_ch3, mut _pwm_ch4) = p
+        .TIM4
+        .pwm(
+            (c1, c2, c3, c4),
+            &mut afio.mapr,
+            50.hz(),
+            clocks,
+            &mut rcc.apb1,
+        );
+        // .0;
+
+    // let max = pwm.get_max_duty();
+
+    // // hprintln!("max duty: {}", max).unwrap();
+    _pwm_ch1.enable();
+    // // asm::bkpt();
+
+    // full
+    _pwm_ch1.set_duty(500.hz());
+    // pwm.set_duty((max*10)/100);
+    // asm::bkpt();
+
+    // // dim
+    // // pwm.set_duty(4266);
+    // pwm.set_duty((max*5)/100);
+    // asm::bkpt();
+
+    // // zero
+    // // pwm.set_duty(2666);
+    // pwm.set_duty((max*8)/100);
+    // asm::bkpt();
 
     loop {}
 }
