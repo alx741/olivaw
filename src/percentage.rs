d1a6552de99077350623e21093e7c2673d7c4bfa
Daniel Campoverde <alx@sillybytes.net>
2019-03-06 16:17:37 -0500

    Percentage data type

---
+++ src/percentage.rs
@@ -0,0 +1,14 @@
+#[derive(Debug)]
+pub struct Percentage {
+    percent: f32,
+}
+
+impl Percentage {
+    pub fn new(percent: f32) -> Percentage {
+        if percent > 0.0 && percent < 100.0 {
+            Percentage { percent }
+        } else {
+            panic!("Percentage value outside [0, 100] range");
+        }
+    }
+}
