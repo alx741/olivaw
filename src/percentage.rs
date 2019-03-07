9d774cc90a5edf4322063d0be84334c2ce2d45c3
Daniel Campoverde <alx@sillybytes.net>
2019-03-07 10:51:43 -0500

    Fix percentage range limits bug

--- src/percentage.rs
+++ src/percentage.rs
@@ -5,7 +5,7 @@ pub struct Percentage {
 
 impl Percentage {
     pub fn new(percent: f32) -> Percentage {
-        if percent > 0.0 && percent < 100.0 {
+        if percent >= 0.0 && percent <= 100.0 {
             Percentage { percent }
         } else {
             panic!("Percentage value outside [0, 100] range");
