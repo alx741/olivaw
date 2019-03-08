01d1b41cfeab740b3c386c119f84b7996ef53d27
Daniel Campoverde <alx@sillybytes.net>
2019-03-08 17:16:08 -0500

    Instantiate motors from PWM channels

--- src/main.rs
+++ src/main.rs
@@ -8,15 +8,23 @@ extern crate cortex_m_rt as rt;
 extern crate panic_semihosting;
 extern crate stm32f1xx_hal as hal;
 
-// use cortex_m_semihosting::hprintln;
+pub mod percentage;
+pub mod propulsion;
 
 // use cortex_m::asm;
+// use cortex_m_semihosting::hprintln;
 use hal::prelude::*;
 use hal::stm32;
+use percentage::Percentage;
+use propulsion::Motors;
 use rt::{entry, exception, ExceptionFrame};
 
 #[entry]
 fn main() -> ! {
+    // let mut motors = Motors::new();
+    // motors.front_right = Percentage::new(50.5);
+    // hprintln!("motors: {:#?}", motors).unwrap();
+
     let p = stm32::Peripherals::take().unwrap();
 
     let mut flash = p.FLASH.constrain();
@@ -24,53 +32,53 @@ fn main() -> ! {
 
     let mut gpioc = p.GPIOC.split(&mut rcc.apb2);
 
-    // #[cfg(feature = "stm32f103")]
     gpioc.pc13.into_push_pull_output(&mut gpioc.crh).set_low();
 
     let clocks = rcc.cfgr.freeze(&mut flash.acr);
 
     let mut afio = p.AFIO.constrain(&mut rcc.apb2);
 
-    // let mut gpioa = p.GPIOA.split(&mut rcc.apb2);
     let mut gpiob = p.GPIOB.split(&mut rcc.apb2);
 
+
     // TIM4
-    let c1 = gpiob.pb6.into_alternate_push_pull(&mut gpiob.crl);
-    let c2 = gpiob.pb7.into_alternate_push_pull(&mut gpiob.crl);
-    let c3 = gpiob.pb8.into_alternate_push_pull(&mut gpiob.crh);
-    let c4 = gpiob.pb9.into_alternate_push_pull(&mut gpiob.crh);
+    let pb6 = gpiob.pb6.into_alternate_push_pull(&mut gpiob.crl);
+    let pb7 = gpiob.pb7.into_alternate_push_pull(&mut gpiob.crl);
+    let pb8 = gpiob.pb8.into_alternate_push_pull(&mut gpiob.crh);
+    let pb9 = gpiob.pb9.into_alternate_push_pull(&mut gpiob.crh);
 
-    let (mut _pwm_ch1, mut _pwm_ch2, mut _pwm_ch3, mut _pwm_ch4) = p
-        .TIM4
-        .pwm(
-            (c1, c2, c3, c4),
-            &mut afio.mapr,
-            50.hz(),
-            clocks,
-            &mut rcc.apb1,
-        );
-        // .0;
+    let tim4_pwm_channels = p.TIM4.pwm(
+        (pb6, pb7, pb8, pb9),
+        &mut afio.mapr,
+        50.hz(),
+        clocks,
+        &mut rcc.apb1,
+    );
+    // .0;
 
-    // let max = pwm.get_max_duty();
+    let mut motors = Motors::new(tim4_pwm_channels);
 
-    // // hprintln!("max duty: {}", max).unwrap();
-    _pwm_ch1.enable();
+
+    // // let max = pwm.get_max_duty();
+
+    // // // hprintln!("max duty: {}", max).unwrap();
+    // _pwm_ch1.enable();
+    // // // asm::bkpt();
+
+    // // full
+    // // _pwm_ch1.set_duty(500.hz());
+    // // pwm.set_duty((max*10)/100);
     // // asm::bkpt();
 
-    // full
-    _pwm_ch1.set_duty(500.hz());
-    // pwm.set_duty((max*10)/100);
-    // asm::bkpt();
+    // // // dim
+    // // // pwm.set_duty(4266);
+    // // pwm.set_duty((max*5)/100);
+    // // asm::bkpt();
 
-    // // dim
-    // // pwm.set_duty(4266);
-    // pwm.set_duty((max*5)/100);
-    // asm::bkpt();
-
-    // // zero
-    // // pwm.set_duty(2666);
-    // pwm.set_duty((max*8)/100);
-    // asm::bkpt();
+    // // // zero
+    // // // pwm.set_duty(2666);
+    // // pwm.set_duty((max*8)/100);
+    // // asm::bkpt();
 
     loop {}
 }
--- src/propulsion.rs
+++ src/propulsion.rs
@@ -4,12 +4,13 @@ use hal::prelude::*;
 use hal::pwm;
 use hal::rcc;
 use hal::stm32;
+use stm32::TIM4;
 
 pub struct Motors {
-    front_right_pwm_ch: pwm::Pwm<stm32::TIM4, pwm::C1>,
-    front_left_pwm_ch: pwm::Pwm<stm32::TIM4, pwm::C2>,
-    back_right_pwm_ch: pwm::Pwm<stm32::TIM4, pwm::C3>,
-    back_left_pwm_ch: pwm::Pwm<stm32::TIM4, pwm::C4>,
+    front_right_pwm_ch: pwm::Pwm<TIM4, pwm::C1>,
+    front_left_pwm_ch: pwm::Pwm<TIM4, pwm::C2>,
+    back_right_pwm_ch: pwm::Pwm<TIM4, pwm::C3>,
+    back_left_pwm_ch: pwm::Pwm<TIM4, pwm::C4>,
 
     pub front_right: Percentage,
     pub front_left: Percentage,
@@ -17,30 +18,21 @@ pub struct Motors {
     pub back_left: Percentage,
 }
 
-impl Motors {
-    pub fn new(
-        pb6: gpiob::PB6<Alternate<PushPull>>,
-        pb7: gpiob::PB7<Alternate<PushPull>>,
-        pb8: gpiob::PB8<Alternate<PushPull>>,
-        pb9: gpiob::PB9<Alternate<PushPull>>,
-        apb1: &mut rcc::APB1,
-        afio: &mut hal::afio::Parts,
-        clocks: rcc::Clocks,
-        tim4: stm32::TIM4,
-    ) -> Motors {
-        let (pwm_ch1, pwm_ch2, pwm_ch3, pwm_ch4) = tim4.pwm(
-            (pb6, pb7, pb8, pb9),
-            &mut afio.mapr,
-            50.hz(),
-            clocks,
-            apb1,
-        );
+// FIXME: Use stm32f1xx_hal::pwm::Pins::Channels associated type instead of this
+type PwmChannels = (
+    pwm::Pwm<TIM4, pwm::C1>,
+    pwm::Pwm<TIM4, pwm::C2>,
+    pwm::Pwm<TIM4, pwm::C3>,
+    pwm::Pwm<TIM4, pwm::C4>,
+);
 
+impl Motors {
+    pub fn new((ch1, ch2, ch3, ch4): PwmChannels) -> Motors {
         Motors {
-            front_right_pwm_ch: pwm_ch1,
-            front_left_pwm_ch:  pwm_ch2,
-            back_right_pwm_ch:  pwm_ch3,
-            back_left_pwm_ch:   pwm_ch4,
+            front_right_pwm_ch: ch1,
+            front_left_pwm_ch: ch2,
+            back_right_pwm_ch: ch3,
+            back_left_pwm_ch: ch4,
             front_right: Percentage::new(0.0),
             front_left: Percentage::new(0.0),
             back_right: Percentage::new(0.0),
