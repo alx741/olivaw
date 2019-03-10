e4bbd3af01241e198337f10d07daf29b1073752f
Daniel Campoverde <alx@sillybytes.net>
2019-03-09 21:52:05 -0500

    Motors initialization

--- src/propulsion.rs
+++ src/propulsion.rs
@@ -1,3 +1,4 @@
+use cortex_m_semihosting::hprintln;
 use crate::percentage::Percentage;
 use hal::gpio::{gpiob, Alternate, PushPull};
 use hal::prelude::*;
@@ -7,15 +8,17 @@ use hal::stm32;
 use stm32::TIM4;
 
 pub struct Motors {
+    pub front_right: Percentage,
+    pub front_left: Percentage,
+    pub back_right: Percentage,
+    pub back_left: Percentage,
+
     front_right_pwm_ch: pwm::Pwm<TIM4, pwm::C1>,
     front_left_pwm_ch: pwm::Pwm<TIM4, pwm::C2>,
     back_right_pwm_ch: pwm::Pwm<TIM4, pwm::C3>,
     back_left_pwm_ch: pwm::Pwm<TIM4, pwm::C4>,
 
-    pub front_right: Percentage,
-    pub front_left: Percentage,
-    pub back_right: Percentage,
-    pub back_left: Percentage,
+    max_speed_duty: u16
 }
 
 // FIXME: Use stm32f1xx_hal::pwm::Pins::Channels associated type instead of this
@@ -27,16 +30,27 @@ type PwmChannels = (
 );
 
 impl Motors {
-    pub fn new((ch1, ch2, ch3, ch4): PwmChannels) -> Motors {
+    pub fn initialize((mut ch1, mut ch2, mut ch3, mut ch4): PwmChannels) -> Motors {
+        ch1.set_duty(0);
+        ch2.set_duty(0);
+        ch3.set_duty(0);
+        ch4.set_duty(0);
+
+        let ch1_max_duty = ch1.get_max_duty();
+
         Motors {
-            front_right_pwm_ch: ch1,
-            front_left_pwm_ch: ch2,
-            back_right_pwm_ch: ch3,
-            back_left_pwm_ch: ch4,
             front_right: Percentage::new(0.0),
             front_left: Percentage::new(0.0),
             back_right: Percentage::new(0.0),
             back_left: Percentage::new(0.0),
+            front_right_pwm_ch: ch1,
+            front_left_pwm_ch: ch2,
+            back_right_pwm_ch: ch3,
+            back_left_pwm_ch: ch4,
+            max_speed_duty: ch1_max_duty
         }
     }
+}
+
+pub fn set(motors: Motors) {
 }
