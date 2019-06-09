7cf96e16773ee04c184812ee5d6a0c768d9eec89
Daniel Campoverde <alx@sillybytes.net>
2019-06-09 14:54:26 -0500

    Not-panicking semantics for Percentage

--- src/percentage.rs
+++ src/percentage.rs
@@ -1,16 +1,16 @@
-// use ufmt::{derive::uDebug, uDebug};
-
-// #[derive(uDebug)]
 pub struct Percentage {
     percent: f32,
 }
 
 impl Percentage {
     pub fn new(percent: f32) -> Percentage {
-        if percent >= 0.0 && percent <= 100.0 {
-            Percentage { percent }
+        if percent < 0.0 {
+            Percentage { percent: 0.0 }
+        }
+        else if percent > 100.0 {
+            Percentage { percent: 100.0 }
         } else {
-            panic!("Percentage value outside [0, 100] range");
+            Percentage { percent }
         }
     }
 
