8f7d8b6a9cbec1b4937aabb486554c8fdb790cd8
Daniel Campoverde <alx@sillybytes.net>
2019-06-09 09:20:03 -0500

    Compile with specs

--- src/main.c
+++ src/main.c
@@ -4,5 +4,12 @@ extern void initialise_monitor_handles(void);
 
 int main(void) {
     initialise_monitor_handles();
+    while (1)
+    {
+        print_this_shit();
+    }
+}
+
+void print_this_shit(void) {
     printf("hello daniel\n");
 }
